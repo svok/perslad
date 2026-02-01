8eb5a84ea20e2b4d149356b30b749a3d33521826
Сергей Окатов <sokatov@gmail.com>
2026-02-01 15:57:51 +0500

    Fix ingestor port

--- ingestor/app/config/runtime.py
+++ ingestor/app/config/runtime.py
@@ -6,7 +6,7 @@ class RuntimeConfig(BaseSettings):
     ENV: str = Field(default="dev")
     LOG_LEVEL: str = Field(default="INFO")
     WORKSPACE_PATH: str = Field(default="/workspace")
-    INGESTOR_PORT: int = Field(default=8001)
+    INGESTOR_PORT: int = Field(default=8124)
     EMBED_URL: str = Field(default="http://emb:8001/v1")
     EMBED_API_KEY: str = Field(default="sk-dummy")
 
